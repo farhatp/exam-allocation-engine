578ed828beceb4e7587148e20a190d0144a24f05
farhatp <39731105+farhatp@users.noreply.github.com>
2025-11-18 17:15:04 +0530

    Delete exam-allocation-engine directory

--- exam-allocation-engine/src/main/java/com/example/examallocation/repository/CenterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.examallocation.repository;
-
-import com.example.examallocation.model.Center;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CenterRepository extends JpaRepository<Center, Long> {
-}
