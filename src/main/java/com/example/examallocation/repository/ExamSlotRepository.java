23effbd0922325455fe6e798b5ffc6d22450f004
farhatp <39731105+farhatp@users.noreply.github.com>
2025-11-21 03:28:52 +0530

    Delete src/main directory

--- src/main/java/com/example/examallocation/repository/ExamSlotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.examallocation.repository;
-
-import com.example.examallocation.model.ExamSlot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamSlotRepository extends JpaRepository<ExamSlot, Long> {
-}
